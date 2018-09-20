78675e5073ef4083543d0fb8c115dfcdaccd34c9
Jon Caldwell <char7f@gmail.com>
2018-09-20 12:31:39 -0700

    Add empty array test, rearrange

--- encoder.go
+++ encoder.go
@@ -207,14 +207,17 @@ func (d *textDecoder) Decode(t string, value []byte) (driver.Value, error) {
 		if len(v) > 0 && v[0] == '[' && v[len(v)-1] == ']' {
 			var items []string
 			subType := t[6 : len(t)-1]
-			// check if array of strings and not empty (['example'])
-			if (subType == "String" || strings.HasPrefix(subType, "FixedString")) && len(v) > 4 {
-				items = strings.Split(v[2:len(v)-2], "','")
-				for i, v := range items {
-					items[i] = unescape(v)
+			// check that array is not empty ([])
+			if len(v) > 2 {
+				// check if array of strings and not empty (['example'])
+				if (subType == "String" || strings.HasPrefix(subType, "FixedString")) && len(v) > 4 {
+					items = strings.Split(v[2:len(v)-2], "','")
+					for i, v := range items {
+						items[i] = unescape(v)
+					}
+				} else {
+					items = strings.Split(v[1:len(v)-1], ",")
 				}
-			} else if len(v) > 2 { // check that array is not empty ([])
-				items = strings.Split(v[1:len(v)-1], ",")
 			}
 
 			r := reflect.MakeSlice(reflect.SliceOf(columnType(subType)), len(items), len(items))
