9767765c9e145c08f7100356b37f4acbbf425684
Oleg Matrokhin <oyumatrokhin@avito.ru>
2018-11-13 18:20:01 +0300

    add flag to regexp for multiline queries

--- stmt_test.go
+++ stmt_test.go
@@ -194,6 +194,18 @@ func (s *stmtSuite) TestFixDoubleInterpolateInStmt() {
 	s.NoError(rows.Close())
 }
 
+func (s *stmtSuite) TestFixMultiLineInsert() {
+	q := `
+INSERT INTO tbl (
+a
+) VALUES (
+?
+)
+`
+	st := newStmt(q)
+	s.True(st.batchMode)
+}
+
 func TestStmt(t *testing.T) {
 	suite.Run(t, new(stmtSuite))
 }
