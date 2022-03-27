788e61ac055a0061d6840d111af9f7608ba914eb
Denis Dorozhkin <ddenis1@yandex.ru>
2022-03-27 23:43:27 +0300

    support parsing map types

--- conn_go18_test.go
+++ conn_go18_test.go
@@ -48,6 +48,7 @@ func (s *connSuite) TestColumnTypes() {
 		"Int64", "UInt64", "Float64", "String", "String", "Array(Int16)", "Array(UInt8)", "Date", "DateTime",
 		"Enum8('one' = 1, 'two' = 2, 'three' = 3)",
 		"Decimal(9, 4)", "Decimal(18, 4)", "Decimal(38, 4)", "Decimal(10, 4)", "IPv4", "IPv6", "FixedString(8)", "LowCardinality(String)",
+		"Map(String, Array(Int64))",
 	}
 	s.Require().Equal(len(expected), len(types))
 	for i, e := range expected {
